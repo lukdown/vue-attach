0582190383426d2bef2c889dddd890da65939e67
seed-yoo <lukdown10@naver.com>
2024-04-15 12:43:29 +0900

    .

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: "D:/javaStudy/attach/api-attach/src/main/resources/static"
 })
